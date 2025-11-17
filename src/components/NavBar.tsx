dd031b65c691d55a40c5a62a35c1bfdb3ea4816c
benmatt1999 <benmatt1999@gmail.com>
2025-11-16 22:59:28 -0500

    add nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+import './NavBar.css'
+
+const NavBar = () => {
+	return (
+		<nav className="navBar">
+				<div>Home</div>
+				<div>About</div>
+				<div>Contact</div>
+		</nav>
+	)
+}
+
+export default NavBar
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+
+const HomePage = () => {
+    return (
+        <>
+            <NavBar/>
+            <h1 className="header">Ben Thorne's Website is in effect!</h1>
+        </>
+    )
+}
+
+export default HomePage
